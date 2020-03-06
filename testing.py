c4ed180ce712062049102955724602b845cbdf04
aakankshadhurandhar <aakankshadhurandhar34@gmail.com>
2020-03-06 22:26:10 +0530

    Add existing file

--- testing.py
+++ testing.py
@@ -4,15 +4,15 @@ import unittest
 class Forvideo(unittest.TestCase):
 
     def setUp(self):
-        print("hello")
+        print "hello"
     
 
  
         
 
     def test_search_is_python_org(self):
-        print("you are beautiful")
+        print "you are beautiful"
     def tearDown(self):
-        print("yeah!!")
+        print "yeah!!" 
 if __name__ == '__main__':
     unittest.main()
