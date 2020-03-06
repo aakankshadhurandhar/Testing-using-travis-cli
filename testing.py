2bc56094496277809a2e8f5a0912e1caabb5f349
Aakanksha Dhurandhar <34390765+aakankshadhurandhar@users.noreply.github.com>
2020-03-06 22:01:03 +0530

    initial commit

---
+++ testing.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class Forvideo(unittest.TestCase):
+
+    def setUp(self):
+        print("hello")
+    
+
+ 
+        
+
+    def test_search_is_python_org(self):
+        print("you are beautiful")
+    def tearDown(self):
+        print("yeah!!")
+if __name__ == '__main__':
+    unittest.main()
