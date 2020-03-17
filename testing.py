c1da47f452f46f4cbbc64396f1a38da09e031852
aakankshadhurandhar <aakankshadhurandhar34@gmail.com>
2020-03-17 10:53:54 +0530

    friends transction

--- testing.py
+++ testing.py
@@ -1,18 +1,71 @@
-import unittest
+
+N = 3
 
 
-class Forvideo(unittest.TestCase):
-
-    def setUp(self):
-        print "hello"
-    
+def getMin(arr): 
+	
+	minInd = 0
+	for i in range(1, N): 
+		if (arr[i] < arr[minInd]): 
+			minInd = i 
+	return minInd 
 
  
-        
+def getMax(arr): 
 
-    def test_search_is_python_org(self):
-        print "you are beautiful"
-    def tearDown(self):
-        print "yeah!!" 
-if __name__ == '__main__':
-    unittest.main()
+	maxInd = 0
+	for i in range(1, N): 
+		if (arr[i] > arr[maxInd]): 
+			maxInd = i 
+	return maxInd 
+
+ 
+def minOf2(x, y): 
+
+	return x if x < y else y 
+
+ 
+def minCashFlowRec(amount): 
+
+	
+	mxCredit = getMax(amount) 
+	mxDebit = getMin(amount) 
+
+	
+	if (amount[mxCredit] == 0 and amount[mxDebit] == 0): 
+		return 0
+
+	# Find the minimum of two amounts 
+	min = minOf2(-amount[mxDebit], amount[mxCredit]) 
+	amount[mxCredit] -=min
+	amount[mxDebit] += min
+
+	# If minimum is the maximum amount to be 
+	print("Person " , mxDebit , " pays " , min
+		, " to " , "Person " , mxCredit) 
+
+	
+	minCashFlowRec(amount) 
+
+
+def minCashFlow(graph): 
+
+	# Create an array amount[], 
+	# initialize all value in it as 0. 
+	amount = [0 for i in range(N)] 
+
+	
+	for p in range(N): 
+		for i in range(N): 
+			amount[p] += (graph[i][p] - graph[p][i]) 
+
+	minCashFlowRec(amount) 
+	
+
+graph = [ [0, 1000, 2000], 
+		[0, 0, 5000], 
+		[0, 0, 0] ] 
+
+minCashFlow(graph) 
+
+
